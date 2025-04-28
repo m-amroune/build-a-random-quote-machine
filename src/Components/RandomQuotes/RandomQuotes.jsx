04c16e29ec984489ad8bbd0e5823c5a11fd2bf2a
Moustapha <amroune.moustapha@gmail.com>
2025-04-28 10:39:36 +0200

    add async loadQuotes function for get quotes from api file

--- src/Components/RandomQuotes/RandomQuotes.jsx
+++ src/Components/RandomQuotes/RandomQuotes.jsx
@@ -1,15 +1,38 @@
 import React from 'react';
 import './RandomQuotes.css'
 import { useState } from 'react';
+import twitter from '../Assets/icons/twitter.png'
 
 const RandomQuotes = () => {
-  const [quote, setQuote] = useState({
-    text: "Education is the most powerful weapon which you can use to change the world",
-    author: "Nelson Mandela"
-  });
+let quotes = [];
+
+const [quote, setQuote] = useState({
+  text: "Education is the most powerful weapon which you can use to change the world",
+  author: "Nelson Mandela"
+});
+
+async function loadQuotes(){
+  const response = await fetch("api/api.json");
+  quotes = await response.json();
+}
+
+
+const random = () =>{
+  const select = quotes[Math.floor(Math.random()*quotes.length)];
+  setQuote(select)
+}
+
+
+ 
+  loadQuotes();
   return (
-    <div>
-      
+    <div id="quote-box">
+      <div id="text">{quote.text}</div>
+      <div id="author"> {quote.author} </div>
+      <div id="new-quote"> </div>
+      <div id="tweet-quote">
+        <img src={twitter} alt="icon twitter" /> </div>
+      <button  onClick={()=> {random()} } >New quote</button>
     </div>
   )
 }
